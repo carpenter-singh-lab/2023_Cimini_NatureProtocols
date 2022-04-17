82c04076eeac463998d1422557b793b0810fecf1
bethac07 <bcimini@broadinstitute.org>
2022-04-17 18:03:48 -0400

    Add percent matching, 2d plots

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -107,6 +107,88 @@ def corr_between_non_replicates(df, n_samples, n_replicates, metadata_compound_n
             null_corr.append(np.nanmedian(corr))  # median replicate correlation
     return null_corr
 
+def correlation_between_modalities(modality_1_df, modality_2_df, modality_1, modality_2, metadata_common, metadata_perturbation):
+    """
+    Compute the correlation between two different modalities.
+    :param modality_1_df: Profiles of the first modality
+    :param modality_2_df: Profiles of the second modality
+    :param modality_1: feature that identifies perturbation pairs
+    :param modality_2: perturbation name feature
+    :param metadata_common: perturbation name feature
+    :param metadata_perturbation: perturbation name feature
+    :return: list-like of correlation values
+    """
+    list_common_perturbation_groups = list(np.intersect1d(list(modality_1_df[metadata_common]), list(modality_2_df[metadata_common])))
+
+    merged_df = pd.concat([modality_1_df, modality_2_df], ignore_index=False, join='inner')
+
+    modality_1_df = merged_df.query('Metadata_modality==@modality_1')
+    modality_2_df = merged_df.query('Metadata_modality==@modality_2')
+
+    corr_modalities = []
+
+    for group in list_common_perturbation_groups:
+        modality_1_perturbation_df = modality_1_df.loc[modality_1_df[metadata_common] == group]
+        modality_2_perturbation_df = modality_2_df.loc[modality_2_df[metadata_common] == group]
+
+        for sample_1 in modality_1_perturbation_df[metadata_perturbation].unique():
+            for sample_2 in modality_2_perturbation_df[metadata_perturbation].unique():
+                modality_1_perturbation_sample_df = modality_1_perturbation_df.loc[modality_1_perturbation_df[metadata_perturbation] == sample_1]
+                modality_2_perturbation_sample_df = modality_2_perturbation_df.loc[modality_2_perturbation_df[metadata_perturbation] == sample_2]
+
+                modality_1_perturbation_profiles = get_featuredata(modality_1_perturbation_sample_df)
+                modality_2_perturbation_profiles = get_featuredata(modality_2_perturbation_sample_df)
+
+                corr = np.corrcoef(modality_1_perturbation_profiles, modality_2_perturbation_profiles)
+                corr = corr[0:len(modality_1_perturbation_profiles), len(modality_1_perturbation_profiles):]
+                corr_modalities.append(np.nanmedian(corr))  # median replicate correlation
+
+    return corr_modalities
+
+
+def null_correlation_between_modalities(modality_1_df, modality_2_df, modality_1, modality_2, metadata_common, metadata_perturbation, n_samples):
+    """
+    Compute the correlation between two different modalities.
+    :param modality_1_df: Profiles of the first modality
+    :param modality_2_df: Profiles of the second modality
+    :param modality_1: "Compound", "ORF" or "CRISPR"
+    :param modality_2: "Compound", "ORF" or "CRISPR"
+    :param metadata_common: feature that identifies perturbation pairs
+    :param metadata_perturbation: perturbation name feature
+    :param n_samples: int
+    :return:
+    """
+    list_common_perturbation_groups = list(np.intersect1d(list(modality_1_df[metadata_common]), list(modality_2_df[metadata_common])))
+
+    merged_df = pd.concat([modality_1_df, modality_2_df], ignore_index=False, join='inner')
+
+    modality_1_df = merged_df.query('Metadata_modality==@modality_1')
+    modality_2_df = merged_df.query('Metadata_modality==@modality_2')
+
+    null_modalities = []
+
+    count = 0
+
+    while count < n_samples:
+        perturbations = random.choices(list_common_perturbation_groups, k=2)
+        modality_1_perturbation_df = modality_1_df.loc[modality_1_df[metadata_common] == perturbations[0]]
+        modality_2_perturbation_df = modality_2_df.loc[modality_2_df[metadata_common] == perturbations[1]]
+
+        for sample_1 in modality_1_perturbation_df[metadata_perturbation].unique():
+            for sample_2 in modality_2_perturbation_df[metadata_perturbation].unique():
+                modality_1_perturbation_sample_df = modality_1_perturbation_df.loc[modality_1_perturbation_df[metadata_perturbation] == sample_1]
+                modality_2_perturbation_sample_df = modality_2_perturbation_df.loc[modality_2_perturbation_df[metadata_perturbation] == sample_2]
+
+                modality_1_perturbation_profiles = get_featuredata(modality_1_perturbation_sample_df)
+                modality_2_perturbation_profiles = get_featuredata(modality_2_perturbation_sample_df)
+
+                corr = np.corrcoef(modality_1_perturbation_profiles, modality_2_perturbation_profiles)
+                corr = corr[0:len(modality_1_perturbation_profiles), len(modality_1_perturbation_profiles):]
+                null_modalities.append(np.nanmedian(corr))  # median replicate correlation
+        count += 1
+
+    return null_modalities
+
 class ZCA_corr(BaseEstimator, TransformerMixin):
     def __init__(self, copy=False):
         self.copy = copy
@@ -247,21 +329,84 @@ def calculate_percent_replicating_Target(batch_path,platelist,sphere=None,suffix
 
     return(prop_95)
 
-def plot_simple_comparison(df,x,hue,order=None,hue_order=None,
-col=None, col_order=None,row=None,row_order=None):
+def calculate_percent_matching_Target(batch_path_1,platelist_1,modality_1, batch_path_2,platelist_2, modality_2,
+sphere=None,suffix = '_normalized_feature_select_negcon.csv.gz'):
+    """
+
+    It doesn't look like sphering was done consistently in previous 
+    analysis of these plates, therefore it is configurable here; either 
+    not done, done at the plate level by passing 'sphere=plate', or 
+    done at the batch level by passing 'sphere=batch'.
+    """
+    n_samples = 10000
+
+    data_dict_1 = {}
+    for plate in platelist_1:
+        plate_df = pd.read_csv(os.path.join(batch_path_1, plate,
+                                            plate+suffix))
+        if sphere == 'plate':
+            plate_df = sphere_plate_zca_corr(plate_df)
+
+        data_dict_1[plate] = plate_df   
+    data_df_1 = pd.concat(data_dict_1, join='inner', ignore_index=True)
+    if modality_1 =='Compounds':
+        data_df_1.rename(columns={'Metadata_target':'Metadata_genes'},inplace=True)
+    data_df_1['Metadata_modality'] = modality_1
+    if sphere == 'batch':
+        data_df_1 = sphere_plate_zca_corr(data_df_1)
+    data_df_1 = remove_negcon_empty_wells(data_df_1)
+
+    data_dict_2 = {}
+    for plate in platelist_2:
+        plate_df = pd.read_csv(os.path.join(batch_path_2, plate,
+                                            plate+suffix))
+        if sphere == 'plate':
+            plate_df = sphere_plate_zca_corr(plate_df)
+
+        data_dict_2[plate] = plate_df   
+    data_df_2 = pd.concat(data_dict_2, join='inner', ignore_index=True)
+    if modality_2 =='Compounds':
+        data_df_2.rename(columns={'Metadata_target':'Metadata_genes'},inplace=True)
+    data_df_2['Metadata_modality'] = modality_2
+    if sphere == 'batch':
+        data_df_2 = sphere_plate_zca_corr(data_df_2)
+    data_df_2 = remove_negcon_empty_wells(data_df_2)
+
+    replicate_corr = list(correlation_between_modalities(data_df_1, data_df_2, modality_1, modality_2, 'Metadata_genes', 'Metadata_broad_sample'))
+    null_corr = list(null_correlation_between_modalities(data_df_1, data_df_2, modality_1, modality_2, 'Metadata_genes', 'Metadata_broad_sample', n_samples))
+
+    prop_95, _, _ = percent_score(null_corr, replicate_corr, how='both')
+
+    return(prop_95)
+
+def plot_simple_comparison(df,x,hue,y='Percent Replicating',order=None,hue_order=None,
+col=None, col_order=None,row=None,row_order=None,jitter=0.25,dodge=True):
     sns.set_style("ticks")
     sns.set_context("paper",font_scale=1.5)
-    g = sns.catplot(data=df, x = x ,y='Percent Replicating', order=order,
+    g = sns.catplot(data=df, x = x ,y = y, order=order,
     hue=hue, hue_order=hue_order, col=col, col_order = col_order, row=row,
-    row_order = row_order, palette='Set1',s=8,linewidth=1,jitter=0.25,
-    alpha=0.9,dodge=True)
+    row_order = row_order, palette='Set1',s=8,linewidth=1,jitter=jitter,
+    alpha=0.9,dodge=dodge)
     labels = []
     orig_labels = list(dict.fromkeys(df[x].values).keys())
     for label in orig_labels:
-        label = str(int(label))
+        if type(label)!= str:
+            label = str(int(label))
         labels.append(textwrap.fill(label, width=45/len(orig_labels),break_long_words=False))
     g.set(ylim=([0,1]))
     g.set_xticklabels(labels=labels,rotation=0)
     plotname = f"../figures/{x}-{hue}.png"
+    g.savefig(plotname,dpi=300)
+    print(f'Saved to {plotname}')
+
+def plot_two_comparisons(df,x='Percent Replicating',y='Percent Matching',hue = None, hue_order=None,
+col=None, col_order=None,row=None,row_order=None,style=None):
+    sns.set_style("ticks")
+    sns.set_context("paper",font_scale=1.5)
+    g = sns.relplot(data=df, x = x ,y= y, hue=hue, hue_order=hue_order, col=col, col_order = col_order, 
+    row=row, row_order = row_order, style = style, palette='Set1',edgecolor='k',alpha=0.9,s=60)
+    g.set(xlim=([0,1]))
+    g.set(ylim=([0,1]))
+    plotname = f"../figures/{x}-{y}-{hue}.png"
     g.savefig(plotname,dpi=300)
     print(f'Saved to {plotname}')
